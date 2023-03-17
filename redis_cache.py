0ae93ab35b33871f202ca3afd2c7440b9355a453
Ubuntu <soaibkhan38@gmail.com>
2023-03-17 19:58:20 +0000

    Remove unnecessary funcs

--- redis_cache.py
+++ redis_cache.py
@@ -60,57 +60,6 @@ class RedisDB:
 		return False if value == 0 else True
 
 
-	@staticmethod
-	async def append_list(key: str , *args: Any) -> bool:
-		"""
-		First argument is "key" wich take name of list as string and args take multiple values
-		to append in that list.
-		( values can be either string , integer, float etc. but it all will get saved in string format)
-		
-		Example: redisdb.append_list("my_list", 1, 2, 3)
-		return 'True' if the operation is successful.
-		"""
-
-		with await redis.multi_exec() as pipe:
-			for arg in args:
-				pipe.rpush(key, arg)
-		return True
-
-
-	@staticmethod
-	async def get_list(key:str ) -> List[str]:
-		"""
-		Take key as argument wich is name of the list.
-		return value of the whole list.
-		"""
-
-		value = await redis.lrange(key, 0, -1)
-		return value
-
-
-	@staticmethod
-	async def remove_element_from_list(key:str, value: Any, remove_all=False ) -> bool:
-		"""
-		Take key as first  argument wich is name of the list and
-		value as second argument to remove that element from list 
-		
-		If remove_all is set to be True then it will delete all the matching elemnet value
-		from the list.
-		
-		If remove_all is set to be False ( default ) then it will delete first matching element value
-		from the list.
-			      
-		return 'True' if the operation is successfull.
-		"""
-
-		count = 0 if remove_all else 1
-		try:
-			await redis.lrem(key, count, value)
-			return True
-		except aioredis.exceptions.ResponseError:
-			return False
-
-
 async def encoder(html: HTMLResponse) -> str:
     return pickle.dumps(html).hex()
 
